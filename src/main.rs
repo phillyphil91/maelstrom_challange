3b67963729297ed953e52de9d1ac9802d868e846
Philip Hüls <philip.huels@googlemail.com>
2023-07-21 10:19:03 +0200

    broadcast first iteration

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 // {"src":"c1","dest":"n1","body":{"type":"init","msg_id":1,"node_id":"n3","node_ids":["n1","n2","n3"]}}
+// {"src":"c1","dest":"n1","body":{"type":"topology","topology":{"n1":["n2","n3"],"n2":["n1"],"n3":["n1"]}}}
 
 #![allow(dead_code)]
-use anyhow::Context;
+use anyhow::{bail, Context};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::io::{StdoutLock, Write};
 use unique_id::{random::RandomGenerator, Generator};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Message {
+struct Message<'a> {
     src: String,
     dest: String,
-    body: Payload,
+    body: Payload<'a>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Payload {
+struct Payload<'a> {
     msg_id: Option<usize>,
     in_reply_to: Option<usize>,
 
     #[serde(flatten)]
-    message_type: MessageType,
+    message_type: MessageType<'a>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum MessageType {
+enum MessageType<'a> {
     Init {
         node_id: String,
         node_ids: Vec<String>,
@@ -37,14 +39,29 @@ enum MessageType {
     EchoOk {
         echo: String,
     },
-    Generate {},
+    Generate,
     GenerateOk {
         id: usize,
+    },
+    Broadcast {
+        broadcast_message: usize,
+    },
+    BroadcastOk,
+
+    Read,
+    #[serde(skip_deserializing)]
+    // skipped here because &vec needed. doesn't impl Deserialize
+    ReadOk {
+        messages: &'a Vec<usize>,
+    },
+    Topology {
+        topology: Value,
     },
 }
 
 struct Node {
     msg_id: usize,
+    broadcast_message: Vec<usize>,
 }
 
 impl Node {
@@ -89,8 +106,8 @@ impl Node {
                 self.msg_id += 1;
                 Ok(())
             }
-            MessageType::InitOk { .. } => todo!(),
-            MessageType::EchoOk { .. } => todo!(),
+            MessageType::InitOk { .. } => bail!("init_ok should not be received by a node"),
+            MessageType::EchoOk { .. } => bail!("echo_ok should not be received by a node"),
             MessageType::Generate {} => {
                 let unique_id = RandomGenerator::default();
                 let response = Message {
@@ -112,7 +129,60 @@ impl Node {
                 self.msg_id += 1;
                 Ok(())
             }
-            MessageType::GenerateOk { .. } => todo!(),
+            MessageType::GenerateOk { .. } => bail!("generate_ok should not be received by a node"),
+            MessageType::Broadcast { broadcast_message } => {
+                self.broadcast_message.push(broadcast_message);
+                let response = Message {
+                    src: message.dest,
+                    dest: message.src,
+                    body: Payload {
+                        msg_id: Some(self.msg_id),
+                        in_reply_to: message.body.msg_id,
+                        message_type: MessageType::BroadcastOk,
+                    },
+                };
+
+                serde_json::to_writer(&mut *stdout, &response).unwrap();
+                stdout
+                    .write_all(b"\n")
+                    .context("couldn't successfully write to stdout")?;
+                self.msg_id += 1;
+                Ok(())
+            }
+            MessageType::BroadcastOk { .. } => {
+                bail!("broadcast_ok should not be received by a node")
+            }
+
+            MessageType::Read { .. } => {
+                let response = Message {
+                    src: message.dest,
+                    dest: message.src,
+                    body: Payload {
+                        msg_id: Some(self.msg_id),
+                        in_reply_to: message.body.msg_id,
+                        message_type: MessageType::ReadOk {
+                            messages: &self.broadcast_message,
+                        },
+                    },
+                };
+
+                serde_json::to_writer(&mut *stdout, &response).unwrap();
+                stdout
+                    .write_all(b"\n")
+                    .context("couldn't successfully write to stdout")?;
+                self.msg_id += 1;
+                Ok(())
+            }
+            MessageType::ReadOk { .. } => {
+                bail!("broadcast_ok should not be received by a node")
+            }
+            MessageType::Topology { .. } => {
+                serde_json::to_writer(&mut *stdout, &message).unwrap();
+                stdout
+                    .write_all(b"\n")
+                    .context("couldn't successfully write to stdout")?;
+                Ok(())
+            }
         }
     }
 }
@@ -124,7 +194,10 @@ fn main() -> anyhow::Result<()> {
 
     let mut stdout = std::io::stdout().lock();
 
-    let mut node_at_start_up = Node { msg_id: 1 };
+    let mut node_at_start_up = Node {
+        msg_id: 1,
+        broadcast_message: vec![],
+    };
     for input in deserialized {
         node_at_start_up.generate_response(
             &mut stdout,
@@ -132,13 +205,4 @@ fn main() -> anyhow::Result<()> {
         )?;
     }
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-
-    #[test]
-    fn test_init() {
-        todo!()
-    }
 }
